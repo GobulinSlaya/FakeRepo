43b26627cc0ad4f1f63d33090711cdb04fff24e4
MatyasRick <owermaty@gmail.com>
2021-02-16 17:59:15 +0100

    rename

--- BestSC/Assets/Scripts/Rotate.cs
+++ BestSC/Assets/Scripts/Rotate.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ritate : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
